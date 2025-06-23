da262853c9c93d390c4175439ba2e4605232160f
WinriseF <3098274040@qq.com>
2025-06-23 17:43:41 +0800

    init

---
+++ src/main/java/com/java/aitest/AitestApplication.java
@@ -0,0 +1,15 @@
+package com.java.aitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.stringtemplate.v4.ST;
+
+@SpringBootApplication
+public class AitestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AitestApplication.class, args);
+    }
+
+}
